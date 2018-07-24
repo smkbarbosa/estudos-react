b52140018f1519eb24f62857d0e0ae24608c4bae
Samuel Barbosa <contato@smkbarbosa.eti.br>
2018-07-24 09:59:53 -0300

    instalando react-router-dom

--- react-dashboard/src/index.js
+++ react-dashboard/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './include/bootstrap'
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('root')
+);
 registerServiceWorker();
